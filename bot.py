43fac0ad5f174b991d4e61235241376020aa4eda
Nolan Birkeland <nolan.birkeland@gmail.com>
2023-08-16 16:31:10 -0700

    Bot now reacting to messages and storing content properly. Also adds framework for weekly exporting of lists

--- bot.py
+++ bot.py
@@ -1,18 +1,32 @@
 import discord
 import json
 import storage
+import schedule
+import asyncio
 
 
 def run_Gordon():
     with open("config.json", "r") as token_file:
         token = json.load(token_file)["token"]
 
+    intents = discord.Intents.default()
+    intents.typing = False
+    intents.presences = False
+    intents.messages = True
+    intents.message_content = True
+
     TOKEN = token
-    client = discord.Client()
+    client = discord.Client(intents=intents)
+
+    async def send_grocery_list():
+        storage.export_list()
 
     @client.event
     async def on_ready():
         print("Gordon is online!")
+
+        # not working yet
+        schedule.every().wednesday.at("12:00").do(asyncio.run, send_grocery_list)
 
     @client.event
     async def on_message(message):
@@ -21,7 +35,7 @@ def run_Gordon():
         if message.author == client.user:
             return
 
-        storage.store_message(message)
+        storage.store_message(message.content)
         await message.add_reaction(react_emoji)
 
     client.run(TOKEN)
--- storage.py
+++ storage.py
@@ -3,13 +3,17 @@ import datetime
 food_requests = []
 
 
-
-
 def store_message(message):
     # stores message in list
-    food_requests.push(message)
+    food_requests.append(message)
+
 
 def export_list():
-# exports list as txt file every wednesday to food stewards using SMS api
+    # exports list as txt file every wednesday to food stewards using SMS api
 
+    with open("shopping-lists/list.txt", "w") as list_file:
+        for item in food_requests:
+            list_file.write(str(item))
+
+    food_requests.clear()
     return
